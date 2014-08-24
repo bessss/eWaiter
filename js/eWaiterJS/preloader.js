6858c91c9f9d674d33204f7e75449ad80df39a12
Bessss <v.bessolstev@gmail.com>
2014-08-24 20:24:50 +0400

    fix preloader

--- js/eWaiterJS/preloader.js
+++ js/eWaiterJS/preloader.js
@@ -25,11 +25,13 @@ function setPreText(text)
 
 function createPreloader()
 {
+  var rnd = '?rnd=' + Math.random(0,100);
   Ext.get('preloader').setVisible(true);
-  this.html = '<img id="podnos" src="images/podnos.gif" style="display: none;position:absolute;top:50%;bottom:0;left:0;right:0;margin:0 auto;z-index: 1001;margin-top: -56px;" />\
+  this.html = '<img id="podnos" src="images/podnos.gif' + rnd + '" style="display: none;position:absolute;top:50%;bottom:0;left:0;right:0;margin:0 auto;z-index: 1001;margin-top: -56px;" />\
                <img id="ovals" src="images/ovals2.gif" style="position:absolute;top:50%;bottom:0;left:0;right:0;margin:0 auto;z-index: 1002;display: none;margin-top: 8px;" />  ';
   $('#preloader').append( this.html );
   Ext.get('podnos').setStyle('display','block');
+  
   this.setOvalsEnable();
 }
 
